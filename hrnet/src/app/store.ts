ed71c648771885db2d70b66215021896e2378f2f
boris_montavon <boris.montavon@gmail.com>
2022-07-29 09:30:04 +0200

    [+] Modal init

--- hrnet/src/app/store.ts
+++ hrnet/src/app/store.ts
@@ -4,18 +4,7 @@ import newEmployeeReducer from '../features/newEmployee/newEmployeeSlice';
 export const store = configureStore({
   reducer: {
     newEmployee: newEmployeeReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        // Ignore these action types
-        ignoredActions: ["newEmployee.setBirthDate", "newEmployee.setStartDate"],
-        // Ignore these field paths in all actions
-        // ignoredActionPaths: ['meta.arg', 'payload.timestamp'],
-        // Ignore these paths in the state
-        // ignoredPaths: ['items.dates', "newEmployee.setBirthDate"],
-      },
-    }),
+  }
 });
 
 export type AppDispatch = typeof store.dispatch;
--- hrnet/src/helpers/newEmployeeFormValidation.ts
+++ hrnet/src/helpers/newEmployeeFormValidation.ts
@@ -1,7 +1,7 @@
 // Validate the form
 
 // Validating first name input
-export const isFirstNameValid = ({firstName}: {firstName: string}) => {
+export const isFirstNameValid = ({firstName}: {"firstName": string}) => {
     const firstNameSpan = document.getElementById("firstNameSpan");
     const hasNumber = /\d/.test(firstName);
 
@@ -19,7 +19,7 @@ export const isFirstNameValid = ({firstName}: {firstName: string}) => {
 };
 
 // Validating last name input
-export const isLastNameValid = ({lastName}: {lastName: string}) => {
+export const isLastNameValid = ({lastName}: {"lastName": string}) => {
     const lastNameSpan = document.getElementById("lastNameSpan");
     const hasNumber = /\d/.test(lastName);
 
@@ -38,9 +38,9 @@ export const isLastNameValid = ({lastName}: {lastName: string}) => {
 };
 
 // Validating birth date input
-export const isBirthDateValid = ({birthDate}: {birthDate: string}) => {
+export const isBirthDateValid = ({serializedBirthDate}: {"serializedBirthDate": string}) => {
     const todayDate = Date.parse(new Date().toLocaleDateString("en-US"));
-    const birthDateParsed = Date.parse(birthDate);
+    const birthDateParsed = Date.parse(serializedBirthDate);
     const birthDateSpan = document.getElementById("birthDateSpan");
 
     if (birthDateParsed >= todayDate) {
@@ -57,9 +57,9 @@ export const isBirthDateValid = ({birthDate}: {birthDate: string}) => {
 };
 
 // Validating start date input
-export const isStartDateValid = ({startDate}: {startDate: string}) => {
+export const isStartDateValid = ({serializedStartDate}: {"serializedStartDate": string}) => {
     const todayDate = Date.parse(new Date().toLocaleDateString("en-US"));
-    const startDateParsed = Date.parse(startDate);
+    const startDateParsed = Date.parse(serializedStartDate);
     const startDateSpan = document.getElementById("startDateSpan");
 
     if (startDateParsed < todayDate) {
@@ -76,7 +76,7 @@ export const isStartDateValid = ({startDate}: {startDate: string}) => {
 };
 
 // Validating street input
-export const isStreetValid = ({street}: {street: string}) => {
+export const isStreetValid = ({street}: {"street": string}) => {
     const streetSpan = document.getElementById("streetSpan");
 
     if (street.length < 2) {
@@ -93,7 +93,7 @@ export const isStreetValid = ({street}: {street: string}) => {
 };
 
 // Validating zip code input
-export const isZipCodeValid = ({zipCode}: {zipCode: string}) => {
+export const isZipCodeValid = ({zipCode}: {"zipCode": string}) => {
     const zipCodeSpan = document.getElementById("zipCodeSpan");
 
     if (zipCode.length < 1 || zipCode === "0") {
@@ -110,7 +110,7 @@ export const isZipCodeValid = ({zipCode}: {zipCode: string}) => {
 };
 
 // Validating city input
-export const isCityValid = ({city}: {city: string}) => {
+export const isCityValid = ({city}: {"city": string}) => {
     const citySpan = document.getElementById("citySpan");
 
     if (city.length < 2) {
@@ -127,7 +127,7 @@ export const isCityValid = ({city}: {city: string}) => {
 };
 
 // Validating city input
-export const isStateValid = (state: {label: string, value: string}) => {
+export const isStateValid = (state: {"label": string, "value": string}) => {
     const stateSpan = document.getElementById("stateSpan");
 
     if (!state.label) {
@@ -144,7 +144,7 @@ export const isStateValid = (state: {label: string, value: string}) => {
 };
 
 // Validating city input
-export const isDepartmentValid = (department: {label: string, value: string}) => {
+export const isDepartmentValid = (department: {"label": string, "value": string}) => {
     const departmentSpan = document.getElementById("departmentSpan");
 
     if (!department.label) {
@@ -158,4 +158,54 @@ export const isDepartmentValid = (department: {label: string, value: string}) =>
     departmentSpan?.classList.remove("block");
 
     return true;
+};
+
+export const isFormValidCheck = (data: {
+    "firstName": string,
+    "lastName": string,
+    "serializedBirthDate": string,
+    "serializedStartDate": string,
+    "street": string,
+    "zipCode": string,
+    "city": string,
+    "state": {
+        "label": string,
+        "value": string
+    },
+    "department": {
+        "label": string,
+        "value": string
+    }
+}) => {
+    let isFormValid = true;
+
+    if (!isFirstNameValid({"firstName": data.firstName})) {
+        isFormValid = false;
+    }
+    if (!isLastNameValid({"lastName": data.lastName})) {
+        isFormValid = false;
+    }
+    if (!isBirthDateValid({"serializedBirthDate": data.serializedBirthDate})) {
+        isFormValid = false;
+    }
+    if (!isStartDateValid({"serializedStartDate": data.serializedStartDate})) {
+        isFormValid = false;
+    }
+    if (!isStreetValid({"street": data.street})) {
+        isFormValid = false;
+    }
+    if (!isZipCodeValid({"zipCode": data.zipCode})) {
+        isFormValid = false;
+    }
+    if (!isCityValid({"city": data.city})) {
+        isFormValid = false;
+    }
+    if (!isStateValid(data.state)) {
+        isFormValid = false;
+    }
+    if (!isDepartmentValid(data.department)) {
+        isFormValid = false;
+    }
+
+    return isFormValid;
 };
